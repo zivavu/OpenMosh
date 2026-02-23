f87d09e89759923b9eed6415f2f546d8ace034d4
Tomasz Kierzenkowski <zivavu@gmail.com>
2026-02-23 16:37:22 +0100

    Clean the project

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
 
-const app = mount(App, {
+mount(App, {
   target: document.getElementById('app')!,
 })
-
-export default app
