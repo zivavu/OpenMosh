bdf9a836e5e5a011ff4642a5418c72388445f175
Tomasz Kierzenkowski <zivavu@gmail.com>
2026-06-12 00:28:21 +0200

    Split luminance extraction out of saliency scoring

--- src/lib/tracking/index.ts
+++ src/lib/tracking/index.ts
@@ -1,4 +1,4 @@
 export * from "./types";
-export { computeSaliency } from "./saliency";
+export { computeSaliency, lumFromRGBA } from "./saliency";
 export { readTrackingParams, syncBoxes, resolveFrame } from "./tracker";
 export { drawTrackingToCanvas } from "./render-tracking";
