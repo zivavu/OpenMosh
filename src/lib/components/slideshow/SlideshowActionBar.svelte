1f49cce0e275d068aaf3d7575767906b29687f58
Tomasz Kierzenkowski <zivavu@gmail.com>
2026-07-12 15:58:55 +0200

    Add 120fps option to slideshow mode

--- src/lib/components/slideshow/SlideshowActionBar.svelte
+++ src/lib/components/slideshow/SlideshowActionBar.svelte
@@ -150,6 +150,7 @@
 					<option value={24}>24</option>
 					<option value={30}>30</option>
 					<option value={60}>60</option>
+					<option value={120}>120</option>
 				</select>
 			</div>
 			<button
