665b8c666cc6ddd6948d5738cc3c364673bf7b34
Tomasz Kierzenkowski <zivavu@gmail.com>
2026-07-23 21:02:13 +0200

    Document segment split shortcuts in slideshow

--- src/lib/components/slideshow/SlideshowActionBar.svelte
+++ src/lib/components/slideshow/SlideshowActionBar.svelte
@@ -66,6 +66,8 @@
 		{
 			title: 'Timeline Editing',
 			shortcuts: [
+				{ keys: ['Dbl-click'], description: 'Create / split segment' },
+				{ keys: ['Ctrl+Click'], description: 'Split segment at cursor' },
 				{ keys: ['Ctrl/Cmd+Z'], description: 'Undo (timeline takes priority)' },
 				{ keys: ['Ctrl/Cmd+Shift+Z', 'Ctrl/Cmd+Y'], description: 'Redo' },
 				{ keys: ['Shift+Drag'], description: 'Rectangle-select boundaries' },
